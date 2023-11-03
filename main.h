1981cffb16e1c3bdf860fc7492758dd960c514ba
ShikoliAmos31 <amosshikoli@gmail.com>
2023-11-03 07:41:27 -0700

    main.h

---
+++ main.h
@@ -0,0 +1,17 @@
+#ifndef SHELL_H
+#define SHELL_H
+
+/* Header files */
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <unistd.h>
+#include <stdbool.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+
+#endif
