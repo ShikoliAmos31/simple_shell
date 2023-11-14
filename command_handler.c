812ccda6a5e39dbcad14435907e5e509c16a1857
ShikoliAmos31 <amosshikoli@gmail.com>
2023-11-14 13:52:58 -0800

    command_handler.c

--- command_handler.c
+++ command_handler.c
@@ -1,4 +1,71 @@
-#include "command_handler.h"
+#include "shell.h"
+
+/**
+ * print_error_message - Print an error message to stderr
+ * @message: The error message to print
+ */
+static void print_error_message(const char *message)
+{
+	write(STDERR_FILENO, message, strlen(message));
+}
+
+/**
+ * handle_child_process_status - Handle the status of the child process
+ * @status: The status of the child process
+ */
+static void handle_child_process_status(int status)
+{
+	if (WIFEXITED(status))
+	{
+		char status_msg[50];
+
+		int len = snprintf(status_msg, sizeof(status_msg),
+				"Child process exited with status %d\n", WEXITSTATUS(status));
+
+		write(STDOUT_FILENO, status_msg, len);
+	}
+	else if (WIFSIGNALED(status))
+	{
+		char signal_msg[40];
+
+		int len = snprintf(signal_msg, sizeof(signal_msg),
+				"Child process terminated by signal %d\n", WTERMSIG(status));
+
+		write(STDOUT_FILENO, signal_msg, len);
+	}
+}
+
+/**
+ * run_child_process - Run the logic for the child process
+ * @command: The command to execute
+ */
+static void run_child_process(char **command)
+{
+	pid_t child_pid = fork();
+
+	if (child_pid == -1)
+	{
+		print_error_message("fork error\n");
+		exit(EXIT_FAILURE);
+	}
+	if (child_pid == 0)
+	{
+		/* Child process */
+		if (execve(command[0], command, NULL) == -1)
+		{
+			print_error_message("execve error\n");
+			_exit(EXIT_FAILURE);
+		}
+	}
+	else
+	{
+		 /* Parent process */
+		int status;
+
+		wait(&status);
+		handle_child_process_status(status);
+	}
+}
 
 /**
  * command_handler - Handle command lines with arguments
@@ -10,60 +77,14 @@ int command_handler(int ac, char **av)
 {
 	if (ac < 2)
 	{
-		char error_msg[] = "Usage: <command> [arguments...]\n";
-
-		write(STDERR_FILENO, error_msg, sizeof(error_msg) - 1);
+		print_error_message("Usage: <command> [arguments...]\n");
 		return (1);
 	}
+	/* Extracting command and arguments */
+	char **command = &av[1];
 
 	/* Forking and executing the command */
-	pid_t child_pid;
-	int status;
+	run_child_process(command);
 
-	child_pid = fork();
-
-	if (child_pid == -1)
-	{
-		char fork_error[] = "fork error\n";
-
-		write(STDERR_FILENO, fork_error, sizeof(fork_error) - 1);
-		return (1);
-	}
-	if (child_pid == 0)
-	{
-		/* Child process */
-		char **args = &av[1];
-
-		if (execve(args[0], args) == -1)
-		{
-			char exec_error[] = "execve error\n";
-
-			write(STDERR_FILENO, exec_error, sizeof(exec_error) - 1);
-			_exit(EXIT_FAILURE);
-		}
-	}
-	else
-	{
-		/* Parent process */
-		wait(&status);
-
-		/* Handling child process status */
-		if (WIFEXITED(status))
-		{
-			char status_msg[50];
-			int len = snprintf(status_msg, sizeof(status_msg),
-					"Child process exited with status %d\n", WEXITSTATUS(status));
-
-			write(STDOUT_FILENO, status_msg, len);
-		}
-		else if (WIFSIGNALED(status))
-		{
-			char signal_msg[40];
-			int len = snprintf(signal_msg, sizeof(signal_msg),
-					"Child process terminated by signal %d\n", WTERMSIG(status));
-
-			write(STDOUT_FILENO, signal_msg, len);
-		}
-	}
 	return (0);
 }
